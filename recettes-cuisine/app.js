a533f76c86da914060f2b2c712916637c7a189cb
Adrien <adrien.moncieu@gmail.com>
2022-12-22 19:32:34 +0100

    post recipe

--- recettes-cuisine/app.js
+++ recettes-cuisine/app.js
@@ -4,6 +4,29 @@ let recipes = require("./recettes.json");
 function handleRequest(req, res) {
   const method = req.method; // Verbe HTTP
   const path = req.url; // Chemin
+  let rawBody = []; // Corps brut (Buffer)
+  let body = null; // Corps exploitable ("parsé")
+
+  // Récupération du body
+  req.on("data", (chunk) => {
+    rawBody.push(chunk);
+  });
+
+  // Traitement de la requête
+  req.on("end", () => {
+    // Parsing du body brut si nécessaire
+    if (rawBody.length > 0) {
+      body = JSON.parse(rawBody);
+    }
+  });
+
+  // Ajouter une recette dans un fichier data/[nom_de_la_recette].json puis répondant un message de confirmation
+  if (path === "/recipes" && method === "POST") {
+    const recipe = body;
+    recipes.push(recipe);
+    response.json(res, 201, {}, { message: "Recette ajoutée" });
+    return;
+  }
 
   //Récupérer une recette aléatoire
   if (path === "/recipes/daily" && method === "GET") {
@@ -15,6 +38,18 @@ function handleRequest(req, res) {
   //Récupérer toute les recettes
   if (path === "/recipes" && method === "GET") {
     response.json(res, 200, {}, recipes);
+    return;
+  }
+
+  //Récupérer une recette par son nom
+  if (path.startsWith("/recipes/") && method === "GET") {
+    const recipeName = path.split("/")[2];
+    const recipe = recipes.find((recipe) => recipe.name === recipeName);
+    if (recipe) {
+      response.json(res, 200, {}, recipe);
+      return;
+    }
+    response.code(res, 404);
     return;
   }
 }
