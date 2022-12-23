5af285887105e2dc319c956ac0e7cbce0a1f9888
Adrien <adrien.moncieu@gmail.com>
2022-12-23 09:45:56 +0100

    delete

--- recettes-cuisine/app.js
+++ recettes-cuisine/app.js
@@ -1,4 +1,7 @@
 const response = require("./lib/response");
+const fs = require("fs");
+const unlink = require("fs").unlink;
+const path = require("path");
 let recipes = require("./recettes.json");
 
 function handleRequest(req, res) {
@@ -21,10 +24,39 @@ function handleRequest(req, res) {
   });
 
   // Ajouter une recette dans un fichier data/[nom_de_la_recette].json puis répondant un message de confirmation
-  if (path === "/recipes" && method === "POST") {
-    const recipe = body;
-    recipes.push(recipe);
-    response.json(res, 201, {}, { message: "Recette ajoutée" });
+  if (path.startsWith("/recipes/") && method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      // Récupérer le nom de la recette dans l'URL
+      const recipeName = path.split("/")[2];
+
+      // Ajouter l'extension ".json" au nom de la recette
+      const fileName = `${recipeName}.json;`;
+
+      // Écrire le contenu de la requête dans le fichier JSON
+      fs.writeFile(`data/${fileName}`, body, (err) => {
+        if (err) {
+          response.code(res, 500);
+          return;
+        }
+      });
+
+      //Vérifier si le fichier a bien été créé
+      fs.stat(`data/${fileName}`, (err, stats) => {
+        if (err || !stats.isFile()) {
+          response.code(res, 500);
+          return;
+        }
+      });
+
+      // Répondre avec un message de confirmation status 201
+      response.json(res, 201, {}, { message: "Recette ajoutée avec succès" });
+    });
     return;
   }
 
@@ -50,6 +82,21 @@ function handleRequest(req, res) {
       return;
     }
     response.code(res, 404);
+    return;
+  }
+
+  //Supprimer une recette par son nom en utilisant la methode DELETE et fs.unlink
+  if (path === "/recipes/" && method === "DELETE") {
+    fs.unlink(`data/${recipeName}.json`, (err) => {
+      if (err) {
+        response.code(res, 500, "Internal Server Error");
+        return;
+      }
+    });
+    response.json(res, 200, {}, { message: "Recette supprimée avec succès" });
+    return;
+  } else {
+    response.code(res, 404, "Not found");
     return;
   }
 }
